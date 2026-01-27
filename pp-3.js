e9de4656a2d83239eb06be8f4f824d86ed010ea4
Prottoy Mankin <prottoy032020@gmail.com>
2026-01-27 13:27:00 +0600

    completed pp-3.js

---
+++ pp-3.js
@@ -0,0 +1,9 @@
+// Use a loop to find the sum of numbers from 1 to 50
+
+let sum = 0;
+
+for (let i = 1; i <= 50; i++) {
+    sum += i;
+}
+
+console.log("The sum of numbers from 1 to 50 is:", sum);
