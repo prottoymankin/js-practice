b7b9055c6414118d17a0de96622a31c9f105abf5
Prottoy Mankin <prottoy032020@gmail.com>
2026-01-28 10:54:19 +0600

    solved pp-21.js

---
+++ pp-21.js
@@ -0,0 +1,6 @@
+// If a given string has either x, replace x by y. if the given string has X, replace it by Y.
+const str1 = "xylo";
+const str2 = "Xavier";
+
+console.log(str1.replaceAll("x", "y")); 
+console.log(str2.replaceAll("X", "Y"));
